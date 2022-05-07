d0b3e311ba06074bea6f80219c82dce042f2524a
Gabriela Leite <nleite.gabriela@gmail.com>
2022-05-07 16:26:21 -0300

    update users model and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_07_191548) do
+ActiveRecord::Schema.define(version: 2022_05_07_192305) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,9 @@ ActiveRecord::Schema.define(version: 2022_05_07_191548) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "first_name"
     t.string "last_name"
+    t.string "zip_code"
+    t.string "house_number"
+    t.text "profile"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
