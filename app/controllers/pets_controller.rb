c793c58b9bb6866859b619f53a9f402435cd6107
Gabriela Leite <nleite.gabriela@gmail.com>
2022-05-14 10:35:58 -0300

    adjust create pet

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -18,12 +18,13 @@ class PetsController < ApplicationController
 
   def create
     @pet = Pet.new(pet_params)
+    @pet.user = current_user
+    authorize @pet
     if @pet.save
       redirect_to pet_path(@pet)
     else
       render :new
     end
-    authorize @pet
   end
 
   def destroy
