3855a4f4535c0662bb7b56b4467ae612dea96d45
Raphael Breitschaft <raphael.breitschaft@gmail.com>
2022-05-19 19:04:10 -0300

    the button to adopt, not redirecting, but still work to be done

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :pets
+  resources :pets do
+    resources :adoptions, only: [:new, :create, :delete]
+  end
   resources :users, only: [:show]
   get 'users/my_pets/:id', to: 'users#my_pets'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
