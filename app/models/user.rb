53343545d703823e793747a557ee17061205263d
Raphael Breitschaft <raphael.breitschaft@gmail.com>
2022-05-19 21:09:49 -0300

    validacoes

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,9 +11,9 @@ class User < ApplicationRecord
   has_many :sent_messages, class_name: "Message", foreign_key: "sender_id"
   has_one_attached :photo
 
-  # validates :first_name, :last_name, :house_number, presence: true
-  # validates :zip_code, presence: true, length: { is: 9 }, format: { with: /"^\d{5}-\d{3}$"/, message: "enter a valid zip code" }
-  # validates :email, presence: true, format: { with: /"^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$"/, message: "enter a valid email" }
-  validates :password, presence: true #, format: { with: /"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"/, message: "should have letters, numbers and at least one special character" }
-  # validates :profile, presence: true, length: { minimum: 6 }
+  validates :first_name, :last_name, presence: true
+  validates :zip_code, presence: true
+  validates :email, presence: true, format: { with: /"^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$"/, message: "enter a valid email"}
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :profile, presence: true, length: { minimum: 6 }
 end
