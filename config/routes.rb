5fa67fce9403a90ec1f27e26ad7f245ca5c6a22e
Raphael Breitschaft <raphael.breitschaft@gmail.com>
2022-05-21 14:43:06 -0300

    butoes de confirmar e cancelar adoçao

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :pets do
-    resources :adoptions, only: [:new, :create, :delete]
+    resources :adoptions, only: [:new, :create]
   end
   resources :users, only: [:show]
-  get 'users/my_pets/:id', to: 'users#my_pets'
+  resources :adoptions, only: [:destroy]
+  patch "pets/:id/adopt", to: "pets#pet_adopted", as: :adoption_confirmed
+  # get 'users/my_pets/:id', to: 'users#my_pets'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
